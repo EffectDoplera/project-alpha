f5d57eaba5252f547fe5ccc426596d2594c9e7a8
Mikhail Danshin <effectdopler0@gmail.com>
2021-02-01 10:37:28 +0300

    RRS1-21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 import classes from './Dialogs.module.css'
 
 const Dialogs = () => {
-  return <div>Dialogs</div>
+  return (
+    <div className={classes.dialogs}>
+      <div className={classes.dialogItems}>
+        <div className={classes.dialog}>Misha</div>
+        <div className={classes.dialog}>Vova</div>
+        <div className={classes.dialog}>Andrey</div>
+        <div className={classes.dialog}>Sasha</div>
+      </div>
+      <div className={classes.messages}>
+        <div className={classes.message}>Hi</div>
+        <div className={classes.message}>Lorem ipsum dolor sit amet.</div>
+        <div className={classes.message}>Lorem, ipsum.</div>
+      </div>
+    </div>
+  )
 }
 
 export default Dialogs
